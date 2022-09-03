8d48ecb71989cb5bb5591fb94f1e92a8ea0b26d2
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2022-09-03 22:57:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,18 +31,15 @@ submit_button.click()
 time.sleep(2)
 
 driver.execute_script("window.stop();")
-
 driver.get('https://hh.ru/applicant/resumes')
-
 time.sleep(2)
-
 #refresh_button = driver.find_element_by_xpath('/html/body/div[5]/div/div[3]/div[1]/div/div/div[1]/div[3]/div[2]/div/div[6]/div/div/div/div[1]/span/button')
-
-
 wait = WebDriverWait(driver, 10)
 delay = 600
+
 os.system('cls')
 print("Поднятие.")
+
 while 1==1:
     try:
         myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#HH-React-Root > div > div.HH-MainContent.HH-Supernova-MainContent > div.main-content > div > div > div.bloko-column.bloko-column_container.bloko-column_xs-4.bloko-column_m-8.bloko-column_l-11 > div.bloko-column.bloko-column_xs-4.bloko-column_s-8.bloko-column_m-8.bloko-column_l-11 > div.bloko-gap.bloko-gap_top.bloko-gap_bottom > div > div.bloko-gap.bloko-gap_top > div > div > div > div:nth-child(1) > span > button")))
@@ -52,11 +49,8 @@ while 1==1:
     except TimeoutException:
         print("Ожидаем.")
 
-
     #wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.applicant-resumes-actions-content > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > button:nth-child(1)'))).click()
-    
     #WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".applicant-resumes-actions-content > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > button:nth-child(1)"))).click()
     #time.sleep(1800)
-
 
 driver.close()
